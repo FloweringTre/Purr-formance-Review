02c89765cffe75b17c2baf9935bc9da0783606db
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-17 15:56:35 -0700

    add splash screen and starting button functionality

---
+++ scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$TextureRect.visible = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_timer_timeout() -> void:
+	$AnimationPlayer.play("fade")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_new_game_button_pressed() -> void:
+	pass # Replace with function body.
