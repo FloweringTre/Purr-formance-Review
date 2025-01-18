69c313a2bc97650687931595c30e35ca79849511
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-17 23:08:33 -0700

    link the first level to the main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -16,4 +16,4 @@ func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_new_game_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/officeSpace.tscn")
