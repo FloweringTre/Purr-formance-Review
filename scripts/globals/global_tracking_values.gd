549b110bb0315a5b0e31db1ffe44a59e01758749
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-19 18:20:32 -0700

    create win and losing conditions

--- scripts/globals/global_tracking_values.gd
+++ scripts/globals/global_tracking_values.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var last_reported_kitty_location : Vector2
+var game_was_played : bool = false
+var game_over : bool = false
+var kitty_caught : bool = false
--- scripts/office_space.gd
+++ scripts/office_space.gd
@@ -1,9 +1,40 @@
 extends Node2D
-@onready var level_time_display: Label = $CanvasLayer/levelTimeDisplay
+@onready var level_time_display: Label = $CanvasLayer/timer/levelTimeDisplay
 @onready var level_timer: Timer = $levelTimer
+@onready var end_level_pop_up: Control = $CanvasLayer/endLevelPopUp
+@onready var title: Label = $CanvasLayer/endLevelPopUp/textConditions/title
+@onready var about_text: Label = $CanvasLayer/endLevelPopUp/textConditions/aboutText
+
+func _ready() -> void:
+	GlobalTrackingValues.game_was_played = true
+	end_level_pop_up.visible = false
 
 func _process(delta: float) -> void:
 	level_time_display.text = str(round(level_timer.time_left))
+	if GlobalTrackingValues.kitty_caught:
+		GlobalTrackingValues.game_over = true
+		game_loss()
+		level_timer.stop()
 
 func _on_level_timer_timeout() -> void:
+	GlobalTrackingValues.game_over = true
+	end_level_pop_up.visible = true
+	title.text = "Congraduations"
+	about_text.text = "You have gotten through the workday without being caught by the supervisor!"
+
+func game_loss() -> void:
+	end_level_pop_up.visible = true
+	title.text = "You've been caught!"
+	about_text.text = "Your office reign of terror has come to an end. :("
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().reload_current_scene()
+	GlobalTrackingValues.kitty_caught = false
+	GlobalTrackingValues.game_over = false
+
+
+func _on_exit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainMenu.tscn")
+	GlobalTrackingValues.kitty_caught = false
+	GlobalTrackingValues.game_over = false
