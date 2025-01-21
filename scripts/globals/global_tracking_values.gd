0f85bedb2f072c8f38567fb1e612d103ec98585c
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-20 22:55:39 -0700

    add conditions to the win state based on to-do list

--- scripts/globals/global_tracking_values.gd
+++ scripts/globals/global_tracking_values.gd
@@ -1,6 +1,61 @@
 extends Node
 
 var last_reported_kitty_location : Vector2
+var kitty_caught : bool = false
+
+signal last_chase
+signal game_won
+signal game_has_been_won
+
 var game_was_played : bool = false
 var game_over : bool = false
-var kitty_caught : bool = false
+var game_paused: bool = false
+signal game_resumed
+
+var kitty_in_bed : bool = false
+var bed_location : Vector2
+
+signal kitty_bappin
+var active_item_location : Vector2
+var trash_spilled : int = 0
+var money_spilled : bool
+var printer_broken : bool
+
+var message : String
+signal set_message
+signal list_active
+
+func game_reset() -> void:
+	last_reported_kitty_location = Vector2(0, 0)
+	kitty_caught= false
+	game_over= false
+	game_paused= false
+	kitty_in_bed = false
+	bed_location = Vector2(0, 0)
+	
+	active_item_location = Vector2(0, 0)
+	trash_spilled = 0
+	money_spilled = false
+	printer_broken = false
+
+func send_message(sent_message : String) -> void:
+	message = sent_message
+	set_message.emit()
+	if message == "":
+		list_active.emit(true)
+	else:
+		list_active.emit(false)
+
+func successful_day():
+	var value = 0
+	if trash_spilled != 3:
+		value += 1
+	if !money_spilled:
+		value += 1
+	if !printer_broken:
+		value += 1
+	
+	if value == 0:
+		return true
+	if value > 0:
+		return false
--- scripts/office_space.gd
+++ scripts/office_space.gd
@@ -1,28 +1,64 @@
 extends Node2D
-@onready var level_time_display: Label = $CanvasLayer/timer/levelTimeDisplay
+@onready var level_time_display: Label = $gameUI/topBar/timer/levelTimeDisplay
 @onready var level_timer: Timer = $levelTimer
-@onready var end_level_pop_up: Control = $CanvasLayer/endLevelPopUp
-@onready var title: Label = $CanvasLayer/endLevelPopUp/textConditions/title
-@onready var about_text: Label = $CanvasLayer/endLevelPopUp/textConditions/aboutText
+var time_left 
+@onready var end_level_pop_up: Control = $gameUI/endLevelPopUp
+@onready var title: Label = $gameUI/endLevelPopUp/textConditions/title
+@onready var about_text: Label = $gameUI/endLevelPopUp/textConditions/aboutText
+@onready var game_ui: CanvasLayer = $gameUI
+@onready var escape_menu: Control = $gameUI/escapeMenu
+@onready var music_player: AudioStreamPlayer = $gameUI/musicPlayer
+@onready var interaction_message: Label = $gameUI/topBar/interactionMessage
+@onready var task_list: Control = $gameUI/topBar/taskList
+@onready var trash_count: Label = $gameUI/topBar/taskList/VBoxContainer/HBoxContainer/trashCount
+
 
 func _ready() -> void:
+	GlobalTrackingValues.game_won.connect(on_game_won)
+	GlobalTrackingValues.set_message.connect(set_message)
+	GlobalTrackingValues.list_active.connect(on_list_active)
 	GlobalTrackingValues.game_was_played = true
 	end_level_pop_up.visible = false
+	game_ui.visible = true
+	music_player.play()
+	on_list_active(true)
 
 func _process(delta: float) -> void:
-	level_time_display.text = str(round(level_timer.time_left))
+	if !GlobalTrackingValues.game_paused:
+		level_time_display.text = str(round(level_timer.time_left))
+	else:
+		level_time_display.text = str(round(time_left))
+	
 	if GlobalTrackingValues.kitty_caught:
 		GlobalTrackingValues.game_over = true
 		game_loss()
 		level_timer.stop()
+		music_player.stop()
+	
+	if Input.is_action_just_pressed("pause"):
+		GlobalTrackingValues.game_paused = true
+		escape_menu.visible = true
+		music_player.stream_paused = true
+		time_left = level_timer.time_left
+		level_timer.stop()
 
 func _on_level_timer_timeout() -> void:
+	GlobalTrackingValues.last_chase.emit()
+	GlobalTrackingValues.send_message("Get to the bed to end the workday!")
+
+func on_game_won() -> void:
+	GlobalTrackingValues.send_message("")
 	GlobalTrackingValues.game_over = true
 	end_level_pop_up.visible = true
-	title.text = "Congraduations"
-	about_text.text = "You have gotten through the workday without being caught by the supervisor!"
+	if GlobalTrackingValues.successful_day():
+		title.text = "Congraduations"
+		about_text.text = "You have gotten through the workday without being caught by the supervisor!"
+	else:
+		title.text = "Oh no..."
+		about_text.text = "You didn't complete your kitty tasks today."
 
 func game_loss() -> void:
+	GlobalTrackingValues.send_message("")
 	end_level_pop_up.visible = true
 	title.text = "You've been caught!"
 	about_text.text = "Your office reign of terror has come to an end. :("
@@ -30,11 +66,29 @@ func game_loss() -> void:
 
 func _on_new_game_button_pressed() -> void:
 	get_tree().reload_current_scene()
-	GlobalTrackingValues.kitty_caught = false
-	GlobalTrackingValues.game_over = false
+	GlobalTrackingValues.game_reset()
 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainMenu.tscn")
-	GlobalTrackingValues.kitty_caught = false
-	GlobalTrackingValues.game_over = false
+	GlobalTrackingValues.game_reset()
+
+
+func _on_continue_button_pressed() -> void:
+	GlobalTrackingValues.game_paused = false
+	GlobalTrackingValues.game_resumed.emit()
+	escape_menu.visible = false
+	level_timer.start(time_left)
+	music_player.stream_paused = false
+
+
+func set_message() -> void:
+	interaction_message.text = GlobalTrackingValues.message
+
+func on_list_active(is_active : bool) -> void:
+	task_list.visible = is_active
+	trash_count.text = str(GlobalTrackingValues.trash_spilled, "/3")
+	$gameUI/topBar/taskList/strikes/safeLine.visible = GlobalTrackingValues.money_spilled
+	$gameUI/topBar/taskList/strikes/printerLine.visible = GlobalTrackingValues.printer_broken
+	if GlobalTrackingValues.trash_spilled == 3:
+		$gameUI/topBar/taskList/strikes/trashLine.visible = true
