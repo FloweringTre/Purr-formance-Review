a58c0f92897768785caa36f42790f575da5da7fe
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-25 00:26:01 -0700

    move music to a global scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var splash_timer: Timer = $splashTimer
+@onready var music_player: AudioStreamPlayer = $musicPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +10,6 @@ func _ready() -> void:
 		splash_timer.start()
 	else:
 		$TextureRect.visible = false
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_timer_timeout() -> void:
 	$AnimationPlayer.play("fade")
