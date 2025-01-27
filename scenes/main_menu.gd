e9ca85788dd21591132667a8d7044d6bc1f12517
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-26 19:57:06 -0700

    clean up the main menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,15 +1,25 @@
 extends Control
 
 @onready var splash_timer: Timer = $splashTimer
-@onready var music_player: AudioStreamPlayer = $musicPlayer
+@onready var diff_level: Label = $VBoxContainer/difficultyContainer/diffLevel
+@onready var diff_about: Label = $VBoxContainer/difficultyContainer/diffAbout
+@onready var difficulty_slider: HSlider = $VBoxContainer/difficultyContainer/difficultySlider
+@onready var exit_button: Panel = $VBoxContainer/buttonsContainer/exitButton
+
+@export var for_web : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	MusicPlayer.main_menu_music()
 	if !GlobalTrackingValues.game_was_played:
 		$TextureRect.visible = true
 		splash_timer.start()
 	else:
 		$TextureRect.visible = false
+	_on_difficulty_slider_drag_ended(true)
+	
+	if for_web:
+		exit_button.set_disabled()
 
 func _on_timer_timeout() -> void:
 	$AnimationPlayer.play("fade")
@@ -19,3 +29,23 @@ func _on_exit_button_pressed() -> void:
 
 func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/officeSpace.tscn")
+
+
+func _on_difficulty_slider_drag_ended(value_changed: bool) -> void:
+	var value = difficulty_slider.value
+	GlobalTrackingValues.difficulty_level = value
+	diff_level.text = GlobalTrackingValues.diffLevels[value]
+	diff_about.text = GlobalTrackingValues.aboutLevels[value]
+
+
+func _on_help_back_button_button_pressed() -> void:
+	$helpPopUp.visible = false
+
+func _on_help_button_button_pressed() -> void:
+	$helpPopUp.visible = true
+
+func _on_goals_back_button_button_pressed() -> void:
+	$goalsPopUp.visible = false
+
+func _on_goals_button_button_pressed() -> void:
+	$goalsPopUp.visible = true
