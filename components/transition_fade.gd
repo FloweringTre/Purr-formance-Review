fd9b1ac1e8942cafa4c12fedfcdfce146ea27660
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-01-29 23:01:22 -0700

    add transition fade with text

--- components/transition_fade.gd
+++ components/transition_fade.gd
@@ -2,11 +2,13 @@ extends CanvasLayer
 
 @onready var color_rect: ColorRect = $ColorRect
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var label: Label = $Label
 
 signal transition_finished
 
 func _ready() -> void:
 	color_rect.visible = false
+	label.visible = false
 	animation_player.animation_finished.connect(on_animation_finished)
 
 
@@ -16,7 +18,15 @@ func on_animation_finished(anim_name) -> void:
 		animation_player.play("fade_normal")
 	elif anim_name == "fade_normal":
 		color_rect.visible = false
+		label.visible = false
 
 func transition() -> void:
 	color_rect.visible = true
 	animation_player.play("fade_dark")
+
+func text_transition(scene_label : String) -> void:
+	label.text = scene_label
+	label.visible = true
+	color_rect.visible = true
+	animation_player.play("fade_dark")
+	
