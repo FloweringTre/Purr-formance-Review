53736f4b96215abe0a7a65dd1a6515e07bc9fe3f
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-02-16 02:44:40 -0700

    add long button option

--- components/button_SMOL.gd
+++ components/button_SMOL.gd
@@ -8,9 +8,12 @@ signal button_pressed()
 
 func _ready() -> void:
 	if long:
-		pass
+		$textContainer.custom_minimum_size.x = 154
+		$ButtonLong.visible = true
 	else:
-		button_label.text = button_text
+		$textContainer.custom_minimum_size.x = 58
+		$ButtonShort.visible = true
+	button_label.text = button_text
 	button_label.add_theme_color_override("font_color", Color(0.306, 0.271, 0.133) )
 
 func _on_button_pressed() -> void:
@@ -23,12 +26,14 @@ func _on_button_button_down() -> void:
 	$textContainer.position.y = 4.5
 
 func set_disabled() -> void:
-	$Button.disabled = true
+	$ButtonShort.disabled = true
+	$ButtonLong.disabled = true
 	_on_button_button_down()
 	button_label.add_theme_color_override("font_color", Color(0.745, 0.612, 0.413) )
 
 
 func reenable_button() -> void:
-	$Button.disabled = false
+	$ButtonShort.disabled = false
+	$ButtonLong.disabled = false
 	_on_button_button_up()
 	%buttonLabel.add_theme_color_override("font_color", Color(0.306, 0.271, 0.133) )
